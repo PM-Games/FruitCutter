9e7b5223535f5834a42956b0391e1acf2e4e4925
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-08-17 16:57:16 -0700

    Might have added trail collisions

--- FruitCutter/Assets/TrailCollisions.cs
+++ FruitCutter/Assets/TrailCollisions.cs
@@ -2,17 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(EdgeCollider2D))]
 public class TrailCollisions : MonoBehaviour
 {
+
+    EdgeCollider2D edgeCollider;
+    LineRenderer myLine;
     // Start is called before the first frame update
     void Start()
     {
-        
+        edgeCollider = this.GetComponent<EdgeCollider2D>();
+        myLine = this.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SetEdgeCollider(myLine);
+    }
+
+    void SetEdgeCollider(LineRenderer lineRenderer)
+    {
+        Transform lineTransform = lineRenderer.transform;
+        List<Vector2> edges = new List<Vector2>();
+
+        for (int point = 0; point < lineRenderer.positionCount; point++)
+        {
+            Vector3 lineRendererPoint = lineRenderer.GetPosition(point);
+            edges.Add(new Vector2(lineRendererPoint.x + lineTransform.position.x, lineRendererPoint.y + lineTransform.position.y));
+        }
+
+        edgeCollider.SetPoints(edges);
     }
 }
