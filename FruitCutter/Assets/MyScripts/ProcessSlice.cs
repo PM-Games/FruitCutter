97412ec66017b17176f562a1b785eb16217898e7
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-08-19 20:55:02 -0700

    Part 1 of trying to fix raycast casting

--- FruitCutter/Assets/MyScripts/ProcessSlice.cs
+++ FruitCutter/Assets/MyScripts/ProcessSlice.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ProcessSlice : MonoBehaviour
 {
+    public GameObject fruitSliceParticleSystem;
+    public ParticleSystem fruitSliceBigSmoke;
+    public ParticleSystem fruitSliceGlowSmoke;
+    public ParticleSystem fruitSliceEmberTrail;
+    public string colorOfFruit;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,18 @@ public class ProcessSlice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+    }
+
+    public void DestroyFruit()
+    {
+        GameObject fruitParticle = Instantiate(fruitSliceParticleSystem, this.gameObject.transform);
+
+        if (colorOfFruit == "Red")
+        {
+            ParticleSystem.MainModule psmain = fruitParticle.transform.GetChild(2).gameObject.GetComponent<ParticleSystem>().main;
+            psmain.startColor = Color.red;
+        }
         
     }
 }
--- FruitCutter/Assets/MyScripts/TrailRaycast.cs
+++ FruitCutter/Assets/MyScripts/TrailRaycast.cs
@@ -14,25 +14,23 @@ public class TrailRaycast : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(camera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, Color.red);
+        Debug.DrawRay(camera.ScreenPointToRay(Input.mousePosition), Color.red);
         if (Input.GetKey(KeyCode.Mouse0))
-        {            
+        {
             Debug.Log("Has Clicked mouse button");
-            float mousePosX = Input.mousePosition.x;
-            float mousePosY = Input.mousePosition.y;
-            Debug.Log("X: " + mousePosX);
-            Debug.Log("Y: " + mousePosY);
-            
-            if (Physics.Raycast(camera.ScreenToWorldPoint(Input.mousePosition), Vector3.forward, out RaycastHit hit))
+            Debug.Log(Input.mousePosition);
+
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit))
             {
                 Debug.Log(hit.collider.gameObject.name);
                 if (hit.collider == true)
                 {
                     Debug.Log("You should have hit something with a collider");
                 }
-                Debug.Log("Raycast position is: " + new Vector3(mousePosX, mousePosY, -2f));
-                // CREATE NEW SCRIPT TO HANDLE THIS:
-                // hit.collider.gameObject.GetComponent<>
+                Debug.Log("Raycast position is: " + Input.mousePosition);
+                hit.collider.gameObject.GetComponent<ProcessSlice>().DestroyFruit();
             }
         }
     }
